d296f98e78b201ff46ac6e8fe8a98e67d82d3fbc
Jonathan Wijaya <jonathanwijaya08@gmail.com>
2023-12-04 20:37:12 +0700

    Tampilan detail dan daftar webinar selesai

--- src/components/Partsipasi.jsx
+++ src/components/Partsipasi.jsx
@@ -11,12 +11,35 @@ import { Link } from "react-router-dom";
 
 const Partsipasi = () => {
   return (
-    <div className="grid grid-cols-2 gap-4">
+    <div className="flex flex-wrap p-8">
 
+      <div className="flex flex-row justify-between w-full">  
+        <img src="./public/img/webinar.jpg" alt="" style={{width:"840px",height:"400px"}}/>
+
+          
       <div>
-      <img src="./public/img/webinar.jpg" alt="" style={{width:"1000px",height:"600px"}}/>
-        <div style={{width:"835px", height:"200px", backgroundColor:"white"}}>
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white" >
+        <div className=" flex flex-col justify-start p-10 m-10"  style={{width:"500px", height:"250px", backgroundColor:"white"}}>
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Fundamental UI/UX design: Wireframing dan Prototyping
+          </h5>
+        <ul>
+        <li className="flex items-center mt"> <MdDateRange /> Jumat, 17 Nov 2023 </li>
+        <li className="flex items-center mt"><MdOutlineAccessTimeFilled /> 14.00 - 16.00 WIB</li>
+        <li className="flex items-center mt" style={{borderBottom:"5px solid #000"}}><RiComputerFill /> IT Development</li>
+      </ul>
+      
+    
+          <div className="flex items-center mt-4" >
+          <img className=" w-9" src="./public/Logo_Infinite.png" alt="logo" />
+          <p className="text-xs ml-2 font-bold">Infinite Learning Indonesia</p>
+        </div>
+        </div>
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-between w-full">
+      <div className="p-8" style={{width:"840px", height:"200px", backgroundColor:"white"}}>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white" >
     Deskripsi
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400" style={{textAlign:"justify"}}>
@@ -24,43 +47,26 @@ const Partsipasi = () => {
 <br />#GrowTogetherWithIL
       </p>
     </div>
-    </div>
-
-      <div className="">
-      <div  style={{width:"405px", height:"200px", backgroundColor:"white"}}>
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      Fundamental UI/UX design: Wireframing dan Prototyping
-      </h5>
-      <ul>
-        <li className="flex items-center mt"> <MdDateRange /> Jumat, 17 Nov 2023 </li>
-        <li className="flex items-center mt"><MdOutlineAccessTimeFilled /> 14.00 - 16.00 WIB</li>
-        <li className="flex items-center mt" style={{borderBottom:"5px solid #000"}}><RiComputerFill /> IT Development</li>
-      </ul>
-      
-    
-      <div className="flex items-center mt-4" >
-          <img className=" w-9" src="./public/Logo_Infinite.png" alt="logo" />
-          <p className="text-xs ml-2 font-bold">Infinite Learning Indonesia</p>
-        </div>
-        </div>
-    
     
 
     
 
-   <div style={{width:"405px", height:"200px", backgroundColor:"white"}}>
+    
+
+   <div className="flex flex-col justify-center p-10 mx-10" style={{width:"505px", height:"200px", backgroundColor:"white"}}>
     <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex items-center mt-4" style={{borderBottom:"5px solid #000"}}>
-    <IoTicketSharp style={{width:"100px", height:"100px"}} />
+    <IoTicketSharp style={{width:"130px", height:"100px"}} />
       Fundamental UI/UX design: Wireframing dan Prototyping
       </h5>
-      <div className="flex gap -10">
-      <h5 className="text-2xl font-tracking-tight text-gray-900 dark:text-white ">Harga Gratis</h5>
+      <div className="flex justify-between">
+      <h5 className="flex  text-2xl font-tracking-tight text-gray-900 dark:text-white ">Harga</h5>
+      <h5 className="flex text-end text-2xl font-tracking-tight text-gray-900 dark:text-white ">Gratis</h5>
       </div>
       <Button className='bg-midBlue'> <Link to={"/webinar"}>Lanjut Ke Detail Pendaftaran</Link></Button>
       
       </div>
-
-    </div>
+      </div>
+   
     
     
     </div>
--- src/components/Webinar.jsx
+++ src/components/Webinar.jsx
@@ -6,17 +6,39 @@ import { Link } from "react-router-dom";
 
 const Webinar = () => {
   return (
-    <div className="grid grid-cols-2 gap-4">
-      
-        <div className="max-w-sm" style={{maxWidth:"900px", margin:"20px"}}>
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white" style={{borderBottom:"5px solid #000"}}>
+    <div className="flex flex-col">
+
+
+
+      <div>
+        <h5 className="text-5xl font-bold tracking-tight text-gray-900 dark:text-white mx-20" style={{width:"870px",borderBottom:"5px solid #000"}}>
             Detail Pendaftaran
             </h5>
+            </div>
+      <div className="flex flex-row justify-evenly items-center">
+        <div className="max-w-sm flex justify-between" style={{maxWidth:"900px"}}>
+        
             <div style={{width:"880px", height:"500px", margin:"20px", padding:"20px", backgroundColor:"white"}}>
                 <h1 style={{fontSize:"30px"}}> Data Diri</h1>
-                <h1 className="flex items-center mt space-x-4" style={{fontSize:"20px", lineHeight:"1.5"}}>Id Transaksi 12345</h1>
-                <h1 className="flex items-center mt" style={{fontSize:"20px", lineHeight:"1.5"}}>Nama Angel Naibaho</h1>
-                <h1 className="flex items-center mt" style={{fontSize:"20px", lineHeight:"1.5", borderBottom:"2px solid #000"}}>Email angelnaibaho@gmail.com</h1>
+
+                <div className="flex justify-between">
+                  <p style={{fontSize:"20px", lineHeight:"1.5"}}>ID Transaksi</p>
+                  <p style={{fontSize:"20px", lineHeight:"1.5"}}>12345</p>
+                </div>
+
+                <div className="flex justify-between">
+                  <p style={{fontSize:"20px", lineHeight:"1.5"}}>Nama</p>
+                  <p style={{fontSize:"20px", lineHeight:"1.5"}}>ID Angel Naibaho</p>
+                </div>
+
+                <div className="flex justify-between">
+                  <p style={{fontSize:"20px", lineHeight:"1.5"}}>Email</p>
+                  <p style={{fontSize:"20px", lineHeight:"1.5"}}>angelnaibaho@gmail.com</p>
+                </div>
+
+
+
+
 
                 <h1 style={{fontSize:"30px", lineHeight:"2"}}>
             Pembayaran
@@ -42,21 +64,27 @@ const Webinar = () => {
 
     
 
-    <div>
-    <Card href="#" className="">
+    <div className="flex flex-col justify-between" style={{width:"400px", height:"300px", margin:"10px", padding:"10px", backgroundColor:"white"}}>
+     
       <h1 style={{fontSize:"20px", borderBottom:"2px solid #000"}}>02:13  | Segera selesaikan pendaftaranmu</h1>
       <h1 className="font-bold mt-1" style={{fontSize:"30px"}}> Detail Harga</h1>
-      <h1 style={{fontSize:"20px",borderBottom:"2px solid #000", lineHeight:"1"}}>Total Bayar Gratis</h1>
+      <div className="flex justify-between" style={{borderBottom:"2px solid #000"}}>
+      <h1 style={{fontSize:"20px",lineHeight:"1"}}>Total Bayar</h1>
+      <h1 style={{fontSize:"20px",lineHeight:"1"}}>Gratis</h1>
+      </div>
       <div className="flex items-center gap-2">
         <Checkbox id="agree" />
         <Label htmlFor="agree" className="flex">Saya setuju dengan Syarat & Ketentuan yang benar di eduweb.&nbsp;</Label>
       </div>
       
       <Button className='bg-midBlue'> <Link to={"/webinar"}>Daftar Webinar</Link></Button>
-    </Card>
+    
     
     </div>
 
+    </div>
+
+    
             
     </div>
   )
