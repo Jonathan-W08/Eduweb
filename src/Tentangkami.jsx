eb1f3cf00f0ee63a70da458e2c24d38b23a099e3
Jonathan Wijaya <jonathanwijaya08@gmail.com>
2023-12-04 20:58:29 +0700

    progress tentang kami

--- src/Tentangkami.jsx
+++ src/Tentangkami.jsx
@@ -6,27 +6,32 @@ import { RiPhoneFindLine } from "react-icons/ri";
 
 const Tentangkami = () => {
   return (
-    <div className="grid grid-cols-2 gap-4" style={{margin:"20px", padding:"20px", backgroundColor:"white"}}>
+    <div className="flex flex-col" style={{backgroundColor:"white"}}>
       <div>
       <h1 className="text-[70px] font-bold text-center" style={{margin:"8rem"}}>EDUWEB MENYEDIAKAN INFORMASI MENGENAI WEBINAR </h1>
       <h1 className="text-[30px] text-center" >Memudahkan dalam mencari informasi mengenai webinar - webinar yang akan diadakan</h1>
       <h1 className="" style={{padding:"3rem"}}>#ChooseYourEducationFindYourWebinar</h1>
       </div>
-<div>
-  
-<h1 className="text-[70px] font-bold text-center" >Mengapa Harus EduWeb? </h1>
 
-<Card href="#" className="max-w-sm">
+      
+<div>
+  <div>
+<h1 className="text-[70px] font-bold text-center" >Mengapa Harus EduWeb? </h1>
+</div>
+<div className="flex flex-row justify-between mt-10">
+
+<Card href="#" className="max-w-sm flex justify-center">
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      <HiOutlineSpeakerphone className="text-center" style={{width:"200px", height:"200px"}} /> 
+      <HiOutlineSpeakerphone className="" style={{width:"200px", height:"200px"}} /> 
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
       Memperluas jangkauan audiens webinarmu dengan membuat promosi lebih efektif dan mudah dicapai.
       </p>
     </Card>
+
     <Card href="#" className="max-w-sm">
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      <FaHandshake  className="text-center" style={{width:"200px", height:"200px"}} /> 
+      <FaHandshake  className="" style={{width:"200px", height:"200px"}} /> 
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
       Bekerjasama dengan mitra-mitra ternama.
@@ -35,19 +40,22 @@ const Tentangkami = () => {
 
     <Card href="#" className="max-w-sm">
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      <RiPhoneFindLine  className="text-center" style={{width:"200px", height:"200px"}} /> 
+      <RiPhoneFindLine  className="" style={{width:"200px", height:"200px"}} /> 
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
       Menemukan webinar dengan mudah berdasarkan kategori yang sesuai dengan minat dan bidang preferensimu.
       </p>
     </Card>
 
-      
+    </div> 
 </div>
 
 <div>
 <h1 className="text-[70px] font-bold text-center" >Sejarah Kami </h1>
 
+</div>
+<div>
+  <img className="w-5 m-10" src="./public/Logo_Eduweb.png" alt="logo" />
 </div>
 
      
