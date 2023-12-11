e7430af8f3d2f4cc48861f5484adf5c002060435
Jonathan-W08 <jonathanwijaya08@gmail.com>
2023-12-11 23:54:08 +0700

    Bahasa indo semua

--- client/src/components/RegisterWebinar.jsx
+++ client/src/components/RegisterWebinar.jsx
@@ -72,7 +72,7 @@ const RegisterWebinar = (props) => {
 
   return (
     <div className="flex-1 bg-whiteBlue w-5/6 min-h-screen p-6">
-      <h1 className="text-3xl font-bold">Webinar Registration</h1>
+      <h1 className="text-3xl font-bold">Daftar Webinar</h1>
       <div className="mt-12">
         <form onSubmit={submitWebinar}>
           <InputRegister
@@ -108,7 +108,7 @@ const RegisterWebinar = (props) => {
 
           <div className="grid grid-cols-2 gap-3">
             <InputRegister
-              title={"Date"}
+              title={"Tanggal"}
               name={"date"}
               placeholder={""}
               required={true}
@@ -118,7 +118,7 @@ const RegisterWebinar = (props) => {
               changeWebinarData={changeWebinarData}
             />
             <InputRegister
-              title={"Time"}
+              title={"Waktu"}
               name={"time"}
               placeholder={"UI/UX Rules"}
               required={true}
@@ -142,7 +142,7 @@ const RegisterWebinar = (props) => {
           </div>
 
           <SelectRegister
-            title={"Category"}
+            title={"Kategori"}
             name={"categories"}
             options={[
               "IT Development",
@@ -160,7 +160,7 @@ const RegisterWebinar = (props) => {
             type="submit"
             className="bg-midBlue text-white px-6 py-2 mt-6 rounded-md"
           >
-            Submit
+            Kirim
           </button>
         </form>
       </div>
